abb3b0d3f35d30f473d34e57487e7861a354e2fd
Tatyjana Ankudo <ankudo@icloud.com>
2022-10-25 15:41:21 +0200

    bento is ready

---
+++ Week_7/service.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+import bentoml
+from bentoml.io import JSON
+from bentoml.io import NumpyNdarray
+
+from pydantic import BaseModel
+
+
+
+model_ref = bentoml.sklearn.get("mlzoomcamp_homework:latest")
+
+model_ref_runner = model_ref.to_runner()
+
+svc = bentoml.Service("service", runners=[model_ref_runner])
+
+@svc.api(input=JSON(), output=NumpyNdarray())
+def classify(UserProfile):
+    prediction = model_ref_runner.predict.run(UserProfile)
+    print("prediction = ", prediction)
+    return({"prediction" : prediction})
